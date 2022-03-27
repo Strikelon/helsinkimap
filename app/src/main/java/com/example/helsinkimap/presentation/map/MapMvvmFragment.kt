d0ebd5cefb5d98681505d09c7ab90199279f30f2
Aleksandr Strikalov <aleksandr.strikalov>
2022-03-27 18:04:50 +0300

    Add google map to the project

--- app/src/main/java/com/example/helsinkimap/presentation/map/MapMvvmFragment.kt
+++ app/src/main/java/com/example/helsinkimap/presentation/map/MapMvvmFragment.kt
@@ -3,17 +3,25 @@ package com.example.helsinkimap.presentation.map
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.example.helsinkimap.R
 import com.example.helsinkimap.databinding.FragmentMapBinding
 import com.example.helsinkimap.presentation.arch.BaseMvvmFragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapMvvmFragment : BaseMvvmFragment() {
+class MapMvvmFragment : BaseMvvmFragment(), OnMapReadyCallback {
 
     override val viewModel: MapViewModel by viewModels()
     private val binding by viewBinding(FragmentMapBinding::inflate)
 
+    private var mMap: GoogleMap? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         activityComponent().inject(this)
         super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
         with(binding) {
             title.setOnClickListener {
                 viewModel.openDetailsScreen()
@@ -22,6 +30,14 @@ class MapMvvmFragment : BaseMvvmFragment() {
     }
 
     override fun observeLiveData() {
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap.apply {
+            setOnMapClickListener {
+                viewModel.handleMapClick(it)
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/helsinkimap/presentation/map/MapViewModel.kt
+++ app/src/main/java/com/example/helsinkimap/presentation/map/MapViewModel.kt
@@ -2,6 +2,7 @@ package com.example.helsinkimap.presentation.map
 
 import com.example.helsinkimap.domain.navigation.MainRouter
 import com.example.helsinkimap.presentation.arch.viewmodel.MvvmViewModel
+import com.google.android.gms.maps.model.LatLng
 import javax.inject.Inject
 
 class MapViewModel @Inject constructor(
@@ -10,5 +11,8 @@ class MapViewModel @Inject constructor(
 
     fun openDetailsScreen() {
         mainRouter.openDetailsScreen()
+    }
+
+    fun handleMapClick(latLng: LatLng) {
     }
 }
